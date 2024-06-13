fae02235f32288c5f7cb4bb9c351d72c16e9c59c
H3nryK <henrykimani89@gmail.com>
2024-06-13 20:47:48 +0300

    hero section

--- frontend/Navbar.js
+++ frontend/Navbar.js
@@ -4,6 +4,19 @@ const navbarCollapse = document.querySelector('.navbar-collapse');
 
 navbarToggler.addEventListener('click', () => {
   navbarCollapse.classList.toggle('show');
+
+  // Update max-height value based on the content height
+  if (navbarCollapse.classList.contains('show')) {
+    const navbarContent = navbarCollapse.querySelector('.navbar-nav');
+    const contentHeight = navbarContent.offsetHeight;
+    navbarCollapse.style.maxHeight = `${contentHeight}px`;
+  } else {
+    // Set max-height to current height before transitioning to 0
+    navbarCollapse.style.maxHeight = `${navbarCollapse.scrollHeight}px`;
+    setTimeout(() => {
+      navbarCollapse.style.maxHeight = '0';
+    }, 200); // Adjust the timeout duration as needed
+  }
 });
 
 // Close mobile navbar when a nav link is clicked
@@ -12,7 +25,11 @@ const navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(link => {
   link.addEventListener('click', () => {
     if (window.innerWidth < 768) {
-      navbarCollapse.classList.remove('show');
+      navbarCollapse.style.maxHeight = `${navbarCollapse.scrollHeight}px`;
+      setTimeout(() => {
+        navbarCollapse.classList.remove('show');
+        navbarCollapse.style.maxHeight = '0';
+      }, 200); // Adjust the timeout duration as needed
     }
   });
 });
---
+++ frontend/Typed.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('.typed', {
+      strings: ["Welcome to MKSU Hackfest 2024", "Unleash Your Coding Potential", "Join Us for a Weekend of Innovation"],
+      typeSpeed: 100,
+      backSpeed: 25,
+      startDelay: 500,
+      backDelay: 1000,
+      loop: true,
+      smartbackspace: true
+    });
+});
